bd8e4e06a057dcdce3566ae24e8fc95b33bbc3a1
천진아 <totter15@naver.com>
2022-05-02 13:55:55 +0900

    feat:Articles 게시물 가져오기

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import RootStack from './screens/RootStack';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient()
+
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <RootStack/>
+      </NavigationContainer>
+    </QueryClientProvider>
+  
+  );
+};
+
+export default App;
